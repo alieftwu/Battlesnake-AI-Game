1cc41dfda0c15a6c4396b91f8b82ae7befbc44f7
Alonso Arreguin <alonsoarreguin2001@gmail.com>
2024-03-14 13:12:09 -0500

    Update main.py
    Coded TODO: Step 1 on main.py

--- starter-snake-python-main/main.py
+++ starter-snake-python-main/main.py
@@ -64,8 +64,24 @@ def move(game_state: typing.Dict) -> typing.Dict:
         is_move_safe["up"] = False
 
     # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
+    board_width = game_state['board']['width']
+    board_height = game_state['board']['height']
+
+    #Check if moving up will be within bounds
+    if my_head["y"] == board_height - 1:
+        is_move_safe["up"] = False
+
+    #Check if moving down will be within bounds
+    if my_head["y"] == 0:
+        is_move_safe["down"] = False
+    
+    #Check if moving left will be within bounds
+    if my_head["x"] == 0:
+        is_move_safe["left"] = False
+    
+    #Check if moving right will be within bounds    
+    if my_head["x"] == board_width - 1:
+        is_move_safe["right"] = False
 
     # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     # my_body = game_state['you']['body']
